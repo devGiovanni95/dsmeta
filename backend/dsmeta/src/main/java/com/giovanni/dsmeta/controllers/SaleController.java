01873cb098f11d4c4ca4ac921a91c55d9f1d25c5
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-16 09:42:59 -0300

    Creating Send of SMS with Twilio Trial

--- backend/dsmeta/src/main/java/com/giovanni/dsmeta/controllers/SaleController.java
+++ backend/dsmeta/src/main/java/com/giovanni/dsmeta/controllers/SaleController.java
@@ -2,6 +2,7 @@ package com.giovanni.dsmeta.controllers;
 
 import com.giovanni.dsmeta.entities.Sale;
 import com.giovanni.dsmeta.services.SaleService;
+import com.giovanni.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,11 +21,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate",defaultValue = "") String minDate,
             @RequestParam(value = "maxDate",defaultValue = "") String maxDate,
             Pageable pageable){
         return service.findSales(minDate,maxDate,pageable);
+    }
+
+    @GetMapping("/notification")
+    public void notifySms(){
+        smsService.sendSms();
     }
 }
